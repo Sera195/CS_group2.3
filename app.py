71ce0f0769e868303ec6d7c0447e0f493f6fb6fe
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-04-01 11:10:43 +0200

    asfd

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
 import streamlit as st
 
 st.write("JEE")
-<<<<<<< HEAD
-
-Print("world")
-Print("world sax")
-hshshshs
-final solution here
-sss
-asdfsadf
-asss
-
-=======
->>>>>>> cb236a99a525389556be39383b25af33023bdc02
