2fe7ae1359e28b845d1f2f9da0f3a1c5ad9436ef
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-04-02 14:23:50 +0200

    j

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import streamlit as st
 import gmaps
 
-# API key for accessing the Google Maps API
 API_KEY = "AIzaSyBpjEKrlbfkGbVUU8LgiaBFVPHvCADIVgY"
 
-# Configure gmaps with your API key
 gmaps.configure(api_key=API_KEY)
 
-# Create a Google Maps figure
 fig = gmaps.figure()
 
-# Display the figure using Streamlit
 st.write(fig)
